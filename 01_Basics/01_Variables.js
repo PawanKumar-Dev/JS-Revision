68e3ddac31f8b09faa191adc48f73c122291dc55
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-03 09:40:42 +0000

    Variable Declaration

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+// In JS there are 3 ways to declare variables.
+
+const accountId = 458079
+
+let userName = "Pawan"
+
+var emaiID = "pawan@gmail.com"
+
+
+// Usage of "var" keyword is not encourged since it has no block or functional scope.
+
+// "let" and "const" are modern way to declare variables. They have a block or functional scope.
+
+
+isPresent = true
+
+// When we don't have an explicit declaration, then "var" keyword is used by default. So avoid such declaration.
