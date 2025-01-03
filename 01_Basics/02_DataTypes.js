2eb87fcafb7db84e9ad291fc4646ed7a2cd5f70e
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-03 09:58:39 +0000

    datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,9 @@
+// Since EcmaScript update there has been some new updates in JS like arrow function, let and const use etc...
+
+/*
+Acc. to offical documentation all datatypes in JS can divided into 2 types based on how data is stored and retrived from computer memory:
+    1. Primitive (String, Number, Boolean, null, undefined, Symbol, BigInt)
+    2. Non-Primitive (Array, Object, Function)
+*/
+
+// Primitive datatypes are stored and retrived from "Stack" memory. Hence refered as "call by value" datatypes. Becoz when we retrive them, we get copy of original value.
