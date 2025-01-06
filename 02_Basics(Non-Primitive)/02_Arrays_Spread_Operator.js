f8e547c203393af780081f825ab6192a7cc514f5
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-06 06:26:45 +0000

    Object

--- 02_Basics(Non-Primitive)/02_Arrays_Spread_Operator.js
+++ 02_Basics(Non-Primitive)/02_Arrays_Spread_Operator.js
@@ -11,3 +11,20 @@ console.log(newArray)
 // ...() spread methods is easier. And spread operator is more used than anything else. And it join more than two arrays unlike concat() methods.
 const newArray1 = [...names, ...emails]
 console.log(newArray1)
+
+
+// Check if a variable is an Array datatype or not with "Array.isArray()" method
+// returns "true"
+console.log(Array.isArray(names))
+
+
+// Similarly you can convert some datatypes into array "Array.from()" method
+console.log(Array.from("Pawan"))
+
+
+// You can combine multiple value into array with "Array.of()" method
+const score1= 100
+const score2= 200
+const score3= 300
+
+console.log(Array.of(score1,score2, score3))
