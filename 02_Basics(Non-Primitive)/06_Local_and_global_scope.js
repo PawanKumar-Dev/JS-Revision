2fa6270f97c53f7a9fc2af196bccc104b20989d1
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-06 11:46:22 +0000

    Scopes

---
+++ 02_Basics(Non-Primitive)/06_Local_and_global_scope.js
@@ -0,0 +1,26 @@
+// "let" and "const" always define local scope. But "var" is global scope.
+// This is why "var" is avoided in modern JS since this can cause override.
+let a = 5
+const b = 7
+var c = 9
+
+// Here we see no difference in execution
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+// But when we go in {} scopes of diffrent elemets such as if-else, functions etc. Things change.
+if(a == 5) {
+    let d = 10
+    const e = 45
+    var f = 15
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+
+// console.log(d)       //Gives ReferenceError. This is becoz "d" is variable with only local scope
+// console.log(e)       //Gives ReferenceError. This is becoz "e" is also variable with only local scope
+console.log(f)          // This works becoz 'var' create global variables.
