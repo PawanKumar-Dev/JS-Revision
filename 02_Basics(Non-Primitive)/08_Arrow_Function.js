00fd539a2f7685c5b3457448198e7fb0ca02da2a
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-14 11:21:38 +0000

    Arrow Function

--- 02_Basics(Non-Primitive)/08_Arrow_Function.js
+++ 02_Basics(Non-Primitive)/08_Arrow_Function.js
@@ -1,10 +1,17 @@
+// "this" keyword refers to current object in JS. But its current object/context can vary a lot depending upon where you execute it.
+// In below context, "this" refers to our object "user"
 const user = {
     username : "Pawan",
     price : 560,
     welcomeMessage : function () {
-        return `Hi ${this.username}, welcome to userpanel`
+        console.log(`Hi ${this.username}, welcome to userpanel`)
     }
 }
 
 // Returns answer correctly
-console.log(user.welcomeMessage())
+user.welcomeMessage()
+
+// But when we use "this" keyword in Node JS environment. It refers to {} or an empty object.
+console.log(this)
+
+// Same if you excute it in browser "this" refers to "Window" object.
