4c22b88d1dde91c1e24b934072bd9609c689cd6b
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-14 11:58:24 +0000

    Arrow Function and This

--- 02_Basics(Non-Primitive)/08_Arrow_Function.js
+++ 02_Basics(Non-Primitive)/08_Arrow_Function.js
@@ -8,10 +8,44 @@ const user = {
     }
 }
 
-// Returns answer correctly
+// Returns answer correctly.
+user.welcomeMessage()
+user.username = "Shayam"
 user.welcomeMessage()
 
 // But when we use "this" keyword in Node JS environment. It refers to {} or an empty object.
 console.log(this)
 
-// Same if you excute it in browser "this" refers to "Window" object.
+// Same if you excute it in Browser environment "this" refers to "Window" object.
+// Now Arrow function are a bit shorter form to Function Expression
+const sum = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(sum(4, 5))
+
+
+// Arrow functions don't have their "this" but they inherit it. In this case it inherit global "this" keyword context of Node JS.
+const myFunc = () => {
+    console.log(this)
+}
+
+myFunc()
+
+// But what if arrow function defined inside an object
+const person = {
+    name : "Pawan",
+    country: "India",
+    greet : function() {
+        console.log(`Welcome ${this.name}`)
+    },
+    greetTwo : () => {
+        console.log(`Welcome Again ${this.name}`)
+    }
+}
+
+// This works properly since this function declaration
+person.greet()
+
+// But arrow function returns "undefined" when used for "this" keyword
+person.greetTwo()
