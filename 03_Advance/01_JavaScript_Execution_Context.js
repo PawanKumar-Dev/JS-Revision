2d458e63718e2619511617d9fdb14822a85997c8
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-16 06:42:54 +0000

    JEC

---
+++ 03_Advance/01_JavaScript_Execution_Context.js
@@ -0,0 +1,6 @@
+/*  JS Execution Context let us understand the environment in which JS is running. Where functions, variables are processed and executed.
+    There are mainly two execution context with one additional which less used.
+    1. Global Execution Context
+    2. Function Execution Context
+    3. Eval Execution Context
+*/
