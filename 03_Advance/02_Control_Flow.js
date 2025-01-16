49c7c0de8ecdbe409dd5208998fdd0ebf9cfb590
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-16 11:29:15 +0000

    If Else

--- 03_Advance/02_Control_Flow.js
+++ 03_Advance/02_Control_Flow.js
@@ -1,7 +1,15 @@
 // Control flow means whether our code is excuted is conditional or not. For this we use if-else. Our code get excuted only if() condition is true.
 // Conditions are evaluted by comparison operators like: <, >, >=, <=, ==, ===
 // Remember single (equalto =) is assignment operator.
+const userLoggedIn = true
 
-if(true) {
+if(userLoggedIn) {
     console.log("Execute")
+}
+
+// Similarly you can a condition for evalution
+if(2 === "2") {
+    console.log("Condition is true")
+} else {
+    console.log("Condition is false")
 }
