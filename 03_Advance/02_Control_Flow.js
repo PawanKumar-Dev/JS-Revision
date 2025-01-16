cdecf79558ee1c5131ccd918fa1f9861c1432319
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-16 11:11:48 +0000

    Control Flow

---
+++ 03_Advance/02_Control_Flow.js
@@ -0,0 +1,7 @@
+// Control flow means whether our code is excuted is conditional or not. For this we use if-else. Our code get excuted only if() condition is true.
+// Conditions are evaluted by comparison operators like: <, >, >=, <=, ==, ===
+// Remember single (equalto =) is assignment operator.
+
+if(true) {
+    console.log("Execute")
+}
