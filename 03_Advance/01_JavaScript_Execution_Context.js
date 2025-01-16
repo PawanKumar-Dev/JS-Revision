c69f6370fdf6e65a562ff3fe848f47d3d33b1015
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-16 07:46:03 +0000

    JEC

--- 03_Advance/01_JavaScript_Execution_Context.js
+++ 03_Advance/01_JavaScript_Execution_Context.js
@@ -1,6 +1,11 @@
 /*  JS Execution Context let us understand the environment in which JS is running. Where functions, variables are processed and executed.
-    There are mainly two execution context with one additional which less used.
+    There are mainly two execution context with one additional which is less used.
     1. Global Execution Context
     2. Function Execution Context
     3. Eval Execution Context
+*/
+
+/*  Every Execution Context process our code in mainly two steps:
+    1. Memory Creation Phase / Creation Phase :  Variables & functions first get allocted memory space as undefined, functions get assigned their defination only. Nothing else.
+    2. Execution Phase: 
 */
