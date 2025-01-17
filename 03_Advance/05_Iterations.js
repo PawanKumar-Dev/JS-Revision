48d4e317629bd5b361d2b11db98663ea1640c218
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-17 06:42:19 +0000

    Iterations

---
+++ 03_Advance/05_Iterations.js
@@ -0,0 +1,6 @@
+// Iterations also called loops. The most basic loop is "for loop".
+// First our index is assigned value. Then condition is checked. Then we jump to block scope where our data is console logged. After that, our index value is incremented. Then we check the condition again.
+// Loop keep running until condition is false. Then we jump out of for loop.
+for(let index = 1; index <= 5; index++) {
+    console.log(index)
+}
