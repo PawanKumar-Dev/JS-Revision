ee1cb35243bea7db3e23d0e2b10cbf963a5c88b2
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-17 07:06:45 +0000

    For Loop

--- 03_Advance/05_Iterations.js
+++ 03_Advance/05_Iterations.js
@@ -1,6 +1,25 @@
-// Iterations also called loops. The most basic loop is "for loop".
-// First our index is assigned value. Then condition is checked. Then we jump to block scope where our data is console logged. After that, our index value is incremented. Then we check the condition again.
-// Loop keep running until condition is false. Then we jump out of for loop.
+/*  Iterations also called loops. The most basic loop is "for loop".
+    First our index is assigned value. Then condition is checked. Then we jump to block scope where our data is console logged. After that, our index value is incremented. Then we check the condition again.
+    Loop keep running until condition is false. Then we jump out of for loop.
+*/
 for(let index = 1; index <= 5; index++) {
-    console.log(index)
+    // console.log(index)
+}
+
+// break: keyword allow you to stop loop at that point.
+for(let i = 1; i < 5 ; i++) {
+    if(i == 3) {
+        console.log("Breaking at 3")
+        break
+    }
+    console.log(i)
+}
+
+// continue: keyword allow you to skip that iteration only once.
+for(let i = 1; i < 5 ; i++) {
+    if(i == 3) {
+        console.log("Skip at 3")
+        continue
+    }
+    console.log(i)
 }
