cc1471c7f147adf8adb49db98a0326c53104ab90
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-18 09:28:11 +0000

    filter loop

--- 03_Advance/09_Filter_Loop.js
+++ 03_Advance/09_Filter_Loop.js
@@ -3,7 +3,17 @@
 // Hence filter is often used to return things based on some condition
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8]
 
-const myFilteredData = myArray.filter((item) => item > 5)
+
+// Short form works because we have put everything in just one. Also known as "implicit return".
+const myFilteredData1 = myArray.filter((item) => item > 5)
+
+
+// When we use {} scope then we must use "return" keyword. Also known as "explicit return".
+const myFilteredData2 = myArray.filter((item) => {
+    if(item > 5)
+    return item
+})
 
 // Returns [ 6, 7, 8 ]
-console.log(myFilteredData)
+console.log(myFilteredData1)
+console.log(myFilteredData2)
