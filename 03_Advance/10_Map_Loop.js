1be6dc171c27471545b7cf8fd7d88c2bca3905db
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-18 10:03:37 +0000

    Map Loop

--- 03_Advance/10_Map_Loop.js
+++ 03_Advance/10_Map_Loop.js
@@ -3,7 +3,10 @@
 
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+// Shortform works because we haven't opened any scope{}. Also known as "implicit return".
 const newArray1 = myArray.map( (item)=> item + 10 )
+
+// When we use {} scope then we must use "return" keyword. Also known as "explicit return".
 const newArray2 = myArray.map( (item)=> {return item + 10} )
 
 console.log(newArray1)
