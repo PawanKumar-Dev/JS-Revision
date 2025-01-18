2f61aaccf101decbee7eb2de7fc47086a01aa687
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-18 10:42:03 +0000

    Reduce Method

---
+++ 03_Advance/12_Reduce_Loop.js
@@ -0,0 +1,13 @@
+// reduce() loop/method also takes a callback method. But reduce specialize in summing up whole arrays and giving you their grand total.
+// Lets assume we want sum total of "mySampleArray" values.
+const mySampleArray = [ 1, 2, 3 ]
+
+
+const mySumTotal1 = mySampleArray.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue
+}, 0)
+
+const mySumTotal2 = mySampleArray.reduce((accumulator, currentValue)=> accumulator + currentValue, 0)
+
+console.log(mySumTotal1)  //Gives us the sum-total of 6
+console.log(mySumTotal2)  //Gives us the sum-total of 6
