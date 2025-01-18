bb2d6e3204cb76d2d6175bd282b1cdcdfe54c73f
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-18 09:46:53 +0000

    Map Loop

--- 03_Advance/09_Filter_Loop.js
+++ 03_Advance/09_Filter_Loop.js
@@ -4,7 +4,7 @@
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
-// Short form works because we have put everything in just one. Also known as "implicit return".
+// Shortform works because we haven't opened any scope{}. Also known as "implicit return".
 const myFilteredData1 = myArray.filter((item) => item > 5)
 
 
@@ -17,3 +17,21 @@ const myFilteredData2 = myArray.filter((item) => {
 // Returns [ 6, 7, 8 ]
 console.log(myFilteredData1)
 console.log(myFilteredData2)
+
+
+// Now let's see how we access data from 'array of objects' using filter() loop. This useful when user use filters in website.
+const arrOfUsers = [
+    { id: 1, name: "Alice", age: 25 },
+    { id: 2, name: "Bob", age: 30 },
+    { id: 3, name: "Charlie", age: 28 },
+    { id: 4, name: "Diana", age: 35 },
+]
+
+// E.g., user request for data about users who are older than 30
+const usersAgeData = arrOfUsers.filter((item)=> item.age >= 30)
+
+const usersIdData = arrOfUsers.filter((item)=> {return item.id >= 3})
+
+
+console.log(usersAgeData)
+console.log(usersIdData)
