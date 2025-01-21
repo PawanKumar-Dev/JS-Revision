da7fc36388fa9e820bafd2cd05a63a4527b08564
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-21 06:26:07 +0000

    Dom

--- 04_DOM/What_is_Dom.js
+++ 04_DOM/What_is_Dom.js
@@ -3,3 +3,13 @@
 
 // DOM (Document Object Model) is like a bridge between your web page and JavaScript.
 // It turns your HTML into an object that JavaScript can work with.
+
+
+//  "document" is part of "window" object in browser. But you can directly access it also with :cosole.dir(document)
+console.log(window)
+console.dir(document)
+
+
+// "getElementById" method in JS can select and manipulate a specific DOM (Document Object Model) element by its unique id attribute.
+const titleData = document.getElementById(title)
+console.log(titleData)
