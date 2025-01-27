f94865925bc0994349ed2595d792ac698c3e68cf
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-27 16:44:57 +0530

    OOP_ Basic

---
+++ 07_OOP/01_OOP_ Basic.js
@@ -0,0 +1,21 @@
+// JavaScript is primarily a prototype-based language, which means that objects can inherit directly from other objects.
+// Concept like classes were introduced in ES6(EcmaScript 2015) only. As such we can make classes, their instances and other class-based things.
+
+
+// OOP or Object Oriented Programming is just another programming paradigm.
+
+
+/*Four core principles of OOP are:
+
+    1. Encapsulation: You encapsulate variables and methods so that unrestricted access is blocked. Only limited access is given to user.
+    Easy to Remember: Think of "PRIVATE APPS". Apps keep their data private.
+
+    2. Polymorphism: This allows one interface to be used for a general class of actions, with specific actions determined at runtime.
+    Easy to Remember: Think of "MAGIC BUTTON". One button, different results.
+
+    3. Inheritance: When one class (subclass/childclass) inherit attributes and methods from another class (superclass/parent class).
+    Easy to Remember: Think of "FAMILY TRAITS". Child inheriting traits from parent.
+
+    4. Abstraction: Hide the complex implementation details and showing only the necessary features of an object.
+    Easy to Remember: Think of "SIMPLE CONTROLS". Use simple controls without worrying about complex details.
+*/
