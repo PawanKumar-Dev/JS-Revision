5c5f2ec2504ad82458c57c4e911c82e66d8d3883
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-31 10:34:49 +0530

    CommonJS Modules Two

---
+++ 08_Modules/03_CommonJS_Modules_Two.js
@@ -0,0 +1,8 @@
+// Import using CommonJS module.
+const { add, multiply } = require("./02_CommonJS_Modules_One")
+
+const resultOne = add(5, 6)
+console.log(resultOne)
+
+const resultTwo = multiply(3, 2)
+console.log(resultTwo)
