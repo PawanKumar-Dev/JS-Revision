bd4f9ba6b284b47928fddb00a265a6e73737a67e
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-31 10:33:16 +0530

    Modules

---
+++ 08_Modules/01_Modules.js
@@ -0,0 +1,21 @@
+// Module is a file containing JS code that is executed in its own scope.
+// Variables, functions, classes, etc., defined in a module aren't accessible outside unless explicitly exported.
+// This encapsulation prevents pollution of the global namespace.
+
+
+// Two module systems:
+
+/*
+  1. CommonJS Modules (used mainly in Node.js)
+    Uses "require" for importing modules.
+    Uses "module.exports" or "exports" for exporting modules.
+    Loads modules synchronously.
+*/
+
+/*
+  2. ES6 Modules (also known as ECMAScript Modules or ESM)
+    Introduced as a standardized module system for JS.
+    Uses "import" and "export" statements.
+    Supports both synchronous and asynchronous loading (in browsers).
+    Can be used in both client-side and server-side applications.
+*/
