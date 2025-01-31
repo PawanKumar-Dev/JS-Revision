0e8374f88f96b7158f68ad6ca1a7f6480ebe486d
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-31 10:44:45 +0530

    ES_Modules_Two

---
+++ 08_Modules/05_ES_Modules_Two.js
@@ -0,0 +1,9 @@
+// If exported using {} braces import same way.
+import { printName, calculateYear } from './04_ES_Modules_One.js'
+
+console.log(printName("Raju"))
+console.log(calculateYear())
+
+
+// Use below pattern in browser.
+// <script type="module" src="app.js"></script>
