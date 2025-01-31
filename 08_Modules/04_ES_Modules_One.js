ad36f12d46c8e6a635cca2bcfd028b4bfe7410e0
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-01-31 10:43:52 +0530

    ES_Modules_One

--- 08_Modules/04_ES_Modules_One.js
+++ 08_Modules/04_ES_Modules_One.js
@@ -1,3 +1,6 @@
+// Export a single "main" value (e.g., a class, function, or object) using "export default"
+// If multiple value, you can use "export" keyword
+
 export const printName = function (name) {
   return `Your name: ${name}`
 }
