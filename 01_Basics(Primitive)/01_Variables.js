fa7be533144b5bf01e3f05b1a3107c425f8a0e74
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-06 17:04:37 +0530

    Update

--- 01_Basics(Primitive)/01_Variables.js
+++ 01_Basics(Primitive)/01_Variables.js
@@ -8,8 +8,10 @@ var emaiID = "pawan@gmail.com"
 
 
 // Usage of "var" keyword is not encourged since it has functional scope(if inside a function) but if outside a function then it has global scope.
+// When declared globally, "var" become properties of the global object(in case of browser Window), increasing the risk of naming conflicts with other scripts or libraries. (this conflict is called Variable leakage)
 
 // "let" and "const" are modern way to declare variables. They have a block scope.
+// When declared in the global scope(i.e. outside of a function or {} scope), "let" and "const" create global variables. However, unlike var, they do not become properties of the global object.
 
 
 isPresent = true
