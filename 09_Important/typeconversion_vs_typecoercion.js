a15c5fce791129ef182ae9ebd926e5121817b8c2
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-07 11:09:04 +0530

    Update

---
+++ 09_Important/typeconversion_vs_typecoercion.js
@@ -0,0 +1,4 @@
+// Type conversion is the broad concept encompassing any transition from one data type to another. And this include Type Coercion as well.
+
+// Type coercion specifically refers to those implicit conversions the language(in our case JavaScript) performs automatically.
+// This results in bugs and so must be avoided. Always focus on explicit conversion.
