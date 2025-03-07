967d4d1186da2483266e767d1764c71d32c15ccb
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-07 10:38:11 +0530

    Update

--- 09_Important/03_TypeConversion.js
+++ 09_Important/03_TypeConversion.js
@@ -1,1 +1,8 @@
+// Datatype conversion can be minefield, especially when it happens implicitly.
+// Since JavaScript is a dynamic type langauge. Type conversion happens automatically. This “magic” can lead to unpredictable behavior. 
 
+// For e.g., Comparing a string and a number might unexpectedly succeed:
+console.log("5" == 5); // true because of implicit conversion
+console.log("5" === 5); // false – the strict equality operator avoids conversion
+
+// When the conversion happens under the hood, you might lose control over how and when your data is changed.
