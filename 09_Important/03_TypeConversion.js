37cb3dbaacc792311b8faffd7cfe1d4f68450e29
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-07 10:54:21 +0530

    Update

--- 09_Important/03_TypeConversion.js
+++ 09_Important/03_TypeConversion.js
@@ -8,3 +8,10 @@ console.log("5" === 5); // false – the strict equality operator avoids convers
 // When the conversion happens under the hood, you might lose control over how and when your data is changed.
 
 // Converting datatypes deliberately (using methods like: parseInt, parseFloat etc) makes intent clear and reduces the chance for errors.
+
+// When converting a floating-point number to an integer ,JS discards the decimal part. And this can cause data loss.
+// For e.g., converting 3.99 to an integer leaves you with 3, which might not be what you intended.
+
+
+// Remember: explicit type conversion is crucial, especially in JS(which loves implicit conversion).
+// By converting explicitly—using functions like parseInt or parseFloat, we minimizing bugs. And pay attention to writing reliable, maintainable code.
