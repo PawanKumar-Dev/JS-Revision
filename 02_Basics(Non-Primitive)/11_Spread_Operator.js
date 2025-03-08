942f3f8cfaaada99c71c66ae31093e9cd594f9d6
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-08 10:21:40 +0530

    Update

--- 02_Basics(Non-Primitive)/11_Spread_Operator.js
+++ 02_Basics(Non-Primitive)/11_Spread_Operator.js
@@ -1,5 +1,6 @@
 /*
-  Used in places where you want to expand or spread an iterable (like an array, or even an object) into individual elements. Examples include:
+  Spread Operator( ...) :- Used in places where you want to expand or spread an iterable (like array, or object) into individual elements.
+  E.g.,
   Function Calls: Expanding an array into individual arguments.
   Array Literals: Combining or copying arrays.
   Object Literals: Merging objects or creating a shallow copy.
