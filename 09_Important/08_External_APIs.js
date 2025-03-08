7640efd5d3a6260a7f53d664dce572ba543858ff
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-08 14:34:35 +0530

    Update

--- 09_Important/08_External_APIs.js
+++ 09_Important/08_External_APIs.js
@@ -4,4 +4,7 @@
 // Web APIs in browsers: Functions like setTimeout, setInterval, fetch for network requests, and DOM event listeners.
 // Node.js APIs: Modules for file system operations, networking, timers, etc.
 
-How They Work: When you call something like setTimeout(callback, delay), JavaScript hands off the task to an external API provided by the browser. This API waits for the specified delay (or completes the network request, etc.) on a separate thread managed by the host environment. Once the operation is complete, the API doesn’t execute the callback immediately—instead, it schedules the callback to be executed later by placing it into the callback queue.
+// How They Work:
+// When we call something like setTimeout(callback, delay), JavaScript hands off the task to an external API provided by the browser.
+// This API waits for the specified delay (or completes the network request, etc.) on a separate thread managed by the host environment.
+// Once the operation is complete, the API doesn’t execute the callback immediately—instead, it schedules the callback to be executed later by placing it into the callback queue.
