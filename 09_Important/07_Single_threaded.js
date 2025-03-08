4a9c967fa2e16d286fd5bda34c874a844b212cc6
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-08 13:18:26 +0530

    Update

--- 09_Important/07_Single_threaded.js
+++ 09_Important/07_Single_threaded.js
@@ -1,2 +1,11 @@
 // When we say JS is single-threaded, it means that it has one main execution thread that processes code. So only one command runs at a time.
-// 
+
+// JavaScript uses a single call stack, meaning that all your synchronous code (functions, loops, etc.) is executed one after the other.
+// There can be no two lines of JavaScript executing simultaneously in the main thread.
+
+// So if one function takes too long—say, a heavy computation—it can block the entire thread. This is why long-running synchronous tasks can freeze the user interface in browsers.
+
+
+
+
+// But 
