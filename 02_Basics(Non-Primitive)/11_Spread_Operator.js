4c9dc46e0eb9734467c7d3e1dcd2cf57288c0bbe
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-08 10:26:00 +0530

    Update

--- 02_Basics(Non-Primitive)/11_Spread_Operator.js
+++ 02_Basics(Non-Primitive)/11_Spread_Operator.js
@@ -1,8 +1,12 @@
-/*
-  Spread Operator( ...) :- Used in places where you want to expand or spread an iterable (like array, or object) into individual elements.
-  E.g.,
-  Function Calls: Expanding an array into individual arguments.
-  Array Literals: Combining or copying arrays.
+// Spread Operator( ...) :- Used in places where you want to expand or spread an iterable (like array, or object) into individual elements.
+// Function Calls: Expanding an array into individual arguments.
+
+function sumOfAllNums(...nums) {
+  nums.map((item) => item += item)
+}
+
+console.log(sumOfAllNums(1, 2, 3))
+
+/*  Array Literals: Combining or copying arrays.
   Object Literals: Merging objects or creating a shallow copy.
 */
-
