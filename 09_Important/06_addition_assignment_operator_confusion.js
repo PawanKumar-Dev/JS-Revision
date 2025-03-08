a802301ea70365c6fe110b99218cb7574a25303c
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-08 11:05:43 +0530

    Update

--- 09_Important/06_addition_assignment_operator_confusion.js
+++ 09_Important/06_addition_assignment_operator_confusion.js
@@ -3,5 +3,18 @@
 let x = 5
 let y = 6
 
-// Returns 11
+// Returns 11 in both cases below.
 console.log(x += y)
+console.log(x)
+
+
+// But (=+) is not an opertor. Instead JS treats it as "Just Assignment with Unary(+)"
+
+// Returns 6 only in both cases.
+console.log(x =+ y)
+console.log(x)
+
+// Above operation shows you only assignment operation was successful.
+// Becoz "+" was treated as seprate entity.
+// Importantly: "+" can trigger "Type Coercion".
+// So avoid using "=+"
