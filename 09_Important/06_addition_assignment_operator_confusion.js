502ece30a7fbd2db47f4839a30b9498ad83cd05a
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-08 10:55:58 +0530

    Update

---
+++ 09_Important/06_addition_assignment_operator_confusion.js
@@ -0,0 +1,7 @@
+// (+=) is called Addition Assignment Operator and is a shorthand for performing addition and assignment in one swoop.
+// a += b;  is equivalent to: a = a + b;
+let x = 5
+let y = 6
+
+//returns 11
+console.log(x += y)
