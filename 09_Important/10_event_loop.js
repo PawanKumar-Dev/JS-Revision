f2bc89f89564045fd06bd21a44e14c8a51879072
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-08 14:43:52 +0530

    Update

---
+++ 09_Important/10_event_loop.js
@@ -0,0 +1,6 @@
+// Event loop is the mechanism that coordinates the execution of Synchronous code (on the call stack) and Asynchronous code (from the callback queue).
+
+// Event loop continuously checks if the call stack is empty (i.e., no code is currently running).
+
+// And when the call stack is free, the event loop takes the first callback from the callback queue and pushes it onto the call stack for execution.
+// And this process enables JS to handle Asynchronous tasks without truly running them concurrently.
