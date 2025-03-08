96fedd33fa7913798e913e157719133773520c87
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-08 17:00:20 +0530

    Update

--- 09_Important/08_External_APIs.js
+++ 09_Important/08_External_APIs.js
@@ -1,4 +1,4 @@
-// In a browser (or even Node.js) environment, JS isn’t the only player. The host environment provides various APIs that run outside the JS engine. These are called "External APIs".
+// In a browser (or even Node.js) environment, JS isn’t the only player. The host environment provides various APIs that run outside the JS engine. These are called "External APIs" or "Web APIs".
 
 // For Example:
 // Web APIs in browsers: Functions like setTimeout, setInterval, fetch for network requests, and DOM event listeners.
@@ -6,6 +6,6 @@
 
 // How They Work:
 // When we call something like setTimeout(callback, delay), JavaScript hands off the task to an external API provided by the browser.
-// This API waits for the specified delay (or completes the network request, etc.) on a separate thread managed by the host environment.
+// This API waits for the specified delay (or completes the network request immediatly, etc.) on a separate thread managed by the host environment.
 // Once the operation is complete, the API doesn’t execute the callback immediately.
-// Instead, it schedules the callback to be executed later by placing it into the callback queue.
+// Instead, Web API places this callback into the "Callback Queue". Where it waits.
