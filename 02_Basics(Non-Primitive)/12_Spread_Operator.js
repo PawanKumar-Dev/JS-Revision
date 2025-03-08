d0a0cb8fb1e99d010bf6c3d6cad200a58547c6f8
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-08 11:53:27 +0530

    Update

--- 02_Basics(Non-Primitive)/12_Spread_Operator.js
+++ 02_Basics(Non-Primitive)/12_Spread_Operator.js
@@ -1,7 +1,13 @@
 // Spread Operator: Used in places where you want to expand or spread an iterable (like array, object) into individual elements.
 
+// Function Calls: Expanding an array into individual arguments when calling a function is common use (...)Spread Operator.
+const arr = [1, 2, 3]
+function multiply(a, b, c) {
+    return a * b * c
+}
+console.log(multiply(...arr))
+
 /*
-Function Calls: Expanding an array into individual arguments.
 Array Literals: Combining or copying arrays.
 Object Literals: Merging objects or creating a shallow copy.
 */
