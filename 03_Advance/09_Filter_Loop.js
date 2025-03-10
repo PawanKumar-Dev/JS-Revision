149b8aeac82007591778eeac1f66960d1dde2912
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-10 11:29:01 +0530

    Update

--- 03_Advance/09_Filter_Loop.js
+++ 03_Advance/09_Filter_Loop.js
@@ -1,5 +1,6 @@
-// filter() loop is similar to forEach() loop since they both take a callback function and then iterate them. Both are inbuilt array methods.
-// But unlike forEach() loop where we excute something, filter() loop return us data.
+// filter() loop is similar to forEach() loop since they both take a callback function and then iterate them.
+// Both are inbuilt array methods.
+// But unlike forEach() loop where we execute something, filter() loop return us data not execute anything.
 // Hence filter is often used to return things based on some condition
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8]
 
