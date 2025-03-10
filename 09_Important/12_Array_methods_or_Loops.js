e32d68ab6b7b126e6500c3d3d27abf65ae7d7d03
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-10 11:42:05 +0530

    Update

---
+++ 09_Important/12_Array_methods_or_Loops.js
@@ -0,0 +1,8 @@
+// reduce(), map(), forEach() are these methods or loops in JS?
+// It can confusing since they are looping through our data like "for loop"
+
+// Answer: reduce(), map(), and forEach() are built-in array methods in JS. They are not control flow constructs like traditional "for" or "while" loops.
+// Using these methods tends to be more "declarative".
+// You describe what you want to achieve (e.g., transformation in map(), accumulation in reduce()) rather than how to iterate through array.
+// So they produce cleaner, more readable code.
+// For e.g., there's no need to manually manage an index or loop counter.
