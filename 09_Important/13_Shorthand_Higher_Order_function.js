51a572e27467421c98be36cd8636bb1f64546ba5
BeProblemSolver <34765751+PawanKumar-Dev@users.noreply.github.com>
2025-03-17 12:10:24 +0530

    Update

--- 09_Important/13_Shorthand_Higher_Order_function.js
+++ 09_Important/13_Shorthand_Higher_Order_function.js
@@ -1,3 +1,10 @@
-// Higher Order Function can be described in shorthand by using Arrow Function syntax.
+// As we know "Higher-Order Function" is function that returns another function (or accepts functions as arguments).
+const handler = function() {
+  return function() {
+    // Empty function (no logic)
+  }
+}
 
+
+// Higher Order Function can be described in shorthand by using Arrow Function syntax.
 const handler = () => () => {}
